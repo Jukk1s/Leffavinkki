0da8d2fa0cccef0e559b60d05c00c62cf9263cb5
eljash <eljas.hirvela@metropolia.fi>
2021-04-21 11:39:09 +0300

    test commit

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const { check, validationResult} = require('express-validator');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.static('public'));
+app.get('/index.htm', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.htm" );
+})
+app.post('/process_post', urlencodedParser,
+    [check('first_name').isLength({ min: 2 }).withMessage("vähintään kaksi merkkiä!"),
+        check('last_name').isLength({ min: 2 }).withMessage("vähintään kaksi merkkiä!"),
+        check('email').isEmail().withMessage("sähköposti on väärän muotoinen!"),
+        check('age').isNumeric().withMessage("ikä tulee olla numeerinen!")],
+    function (req, res) {
+
+        //Ilmoitetaan jos virheellistä syöttöä
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        }
+
+        // Prepare output in JSON format
+        response = {
+            first_name:req.body.first_name,
+            last_name:req.body.last_name,
+            email:req.body.email,
+            age:req.body.age
+        };
+        console.log(response);
+        res.end(JSON.stringify(response));
+    })
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
