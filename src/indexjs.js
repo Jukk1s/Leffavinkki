7317aa54480ab5e33d41d8727f5c813f21d9f18d
Yucc1s <jukkahal@metropolia.fi>
2021-04-22 22:03:25 +0300

    fetchin tulokset sivulle näkyviin. pientä tyylittelyä/kuvien skaalautumista

--- src/indexjs.js
+++ src/indexjs.js
@@ -1,4 +1,6 @@
 function getMovies() {
+    //document.getElementById("resultField").innerHTML = "";
+
     let name = document.getElementById("movie_name").value.replace(/  +/g, '%20');
     //name.replace(/\s/g, '')
     let year = Number(document.getElementById("movie_year").value);
@@ -18,10 +20,27 @@ function getMovies() {
             if(response){
                 const jsonResponse = await response.json();
                 console.log(jsonResponse);
+                showResults(jsonResponse);
             }
         }catch(error){
             console.log(error);
         }
     })()
+}
 
+function showResults(jsonResponse) {
+    // console.log(Object.keys(jsonResponse.Search).length);
+
+    const div = document.getElementById("resultField");
+    for (let i = 0; i < Object.keys(jsonResponse.Search).length; i++) {
+        let h = document.createElement("h3");
+        h.innerHTML = jsonResponse.Search[i].Title + " (" + jsonResponse.Search[i].Year + ")";
+        div.appendChild(h);
+
+        let img = document.createElement("img");
+        img.src = jsonResponse.Search[i].Poster;
+        img.alt = "Poster of " + jsonResponse.Search[i].Title;
+        div.appendChild(img);
+
+    }
 }
