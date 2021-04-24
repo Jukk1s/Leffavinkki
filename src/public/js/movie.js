d8960491f0a1b0df9e74b846851f77305ca8b52c
eljash <eljas.hirvela@metropolia.fi>
2021-04-24 15:01:33 +0300

    Elokuvan kommenttien haku palauttaa kommentit ja tulostaa ne konsoliin.

--- src/public/js/movie.js
+++ src/public/js/movie.js
@@ -1,3 +1,4 @@
+const nodeServer = "http://localhost:8081";
 const apiurl = "http://www.omdbapi.com/?r=json&i=";
 let apiKey = "&apikey=bfbd237f";
 /*
@@ -5,38 +6,83 @@ let data = localStorage.getItem("movieData");
 let jsonData = JSON.parse(data);
 */
 
+//urlin perässä pitää olla ?id=elokuvanid
 function showMovie(){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    console.log(queryString);
-    console.log(urlParams);
-    try{
-        const movieID = urlParams.get('id');
-        (async () => {
-            console.log(apiurl + movieID + apiKey);
-            try{
-                const response = await fetch(apiurl + movieID + apiKey);
-                if(response){
-                    const jsonResponse = await response.json();
-                    console.log(jsonResponse);
-                    localStorage.setItem("movieData", JSON.stringify(jsonResponse));
-                    showResult(jsonResponse);
+    if(urlParams.get('id')) {
+        try {
+            const movieID = urlParams.get('id');
+            (async () => {
+                //console.log(apiurl + movieID + apiKey);
+                try {
+                    const response = await fetch(apiurl + movieID + apiKey);
+                    if (response) {
+                        const jsonResponse = await response.json();
+                        //console.log(JSON.stringify(jsonResponse));
+                        if (jsonResponse.hasOwnProperty('imdbID')) {
+                            localStorage.setItem("movieData", JSON.stringify(jsonResponse));
+                            showResult(jsonResponse);
+                        } else {
+                            noResult(movieID);
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
                 }
-            }catch(error){
-                console.log(error);
-            }
-        })()
-    } catch (err){
-        console.log(err);
+            })()
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        noResult('no id give!!!');
     }
 }
 
 function showResult(data){
-    console.log(data);
+    //console.log(data);
     const h = document.getElementById("title");
     const img = document.getElementById("poster");
     h.innerHTML = data.Title;
     img.src = data.Poster;
     img.alt = "Poster of " + data.Title;
+    showComments(data.imdbID);
 }
 
+//showcomments?id=elokuvanid
+function showComments(id){
+
+    try {
+        (async () => {
+            console.log("Searching for comments: "+nodeServer+"/showcomments?id="+id);
+            try {
+                const response = await fetch(nodeServer+"/showcomments?id="+id);
+                if (response) {
+                    const jsonResponse = await response.json();
+                    console.log(jsonResponse);
+                    for(var i = 0; i < jsonResponse.length; i++){
+                        if(jsonResponse[i].hasOwnProperty('id')) {
+                            let reviewID = jsonResponse[i].reviews_id;
+                            let commentID = jsonResponse[i].id;
+                            let header = jsonResponse[i].header;
+                            let comment = jsonResponse[i].comment;
+                            let date = jsonResponse[i].date;
+                            console.log(reviewID+", "+commentID,+", "+header+", "+comment+", "+date);
+                        }
+                    }
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        })()
+    } catch (err) {
+        console.log(err);
+    }
+    console.log();
+}
+
+function noResult(id){
+
+    console.error('Could not find movie with id: '+id);
+    //Joku error ilmoitus jos ei löydy
+}
--- src/server.js
+++ src/server.js
@@ -12,6 +12,8 @@ var cors = require('cors');
 
 var path = require('path');
 
+var http = require('http');
+
 app.use(cors());
 
 // Create application/x-www-form-urlencoded parser
@@ -125,6 +127,55 @@ app.post('/showmovie',cors(), function(req,res){
     //var year = q.y;
 
 });
+
+//showcomments?id=elokuvanid
+app.get('/showcomments', cors(), (req,res)=>{
+    var q = url.parse(req.url, true).query;
+    const movieId = q.id;
+    var sql = "SELECT * FROM reviews WHERE movie_id = ?";
+    var string;
+    (async () => {
+        try {
+            const rows = await query(sql, [movieId]);
+
+            string = JSON.stringify(rows);
+
+            sql = "SELECT * FROM comments WHERE reviews_id = ?";
+
+            var returnArray = [];
+
+            //console.log(string);
+            for(var i = 0; i < rows.length; i++){
+                if(rows[i].hasOwnProperty('id')) {
+                    const rows2 = await query(sql, rows[i].id);
+                    for(var l = 0; l < rows2.length; l++){
+                        returnArray.push(rows2[l]);
+                    }
+
+                    /*
+                    if(JSON.stringify(returnArray) === "{}")
+                        returnArray = rows2;
+                    else
+                        returnArray.push(rows2);
+
+                     */
+                    string = JSON.stringify(rows2);
+                    //console.log(string);
+                }
+            }
+            console.log(returnArray);
+            res.json(returnArray);
+
+        }
+        catch (err){
+            console.log("Database error!"+err);
+        }
+        finally {
+
+        }
+    })();
+})
+
 /*app.post('/process_post', urlencodedParser,
     [check('first_name').isLength({ min: 2 }).withMessage("vähintään kaksi merkkiä!"),
         check('last_name').isLength({ min: 2 }).withMessage("vähintään kaksi merkkiä!"),
